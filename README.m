51707e0d3257cfc9579f1834a88a4d17e0188381
Jake <Jake.Laoyu@gmail.com>
2020-12-09 15:45:18 +0800

    Update and rename README.m to README.md

--- README.m
+++
@@ -1,1 +0,0 @@
-<img align="right" src="https://github-readme-stats.vercel.app/api?username=jakelaoyu&show_icons=true&text_color=24292e&bg_color=ffffff&hide_title=true">
---
+++ README.md
@@ -0,0 +1,1 @@
+![](https://github-readme-stats.vercel.app/api?username=jakelaoyu&show_icons=true&text_color=24292e&bg_color=ffffff&hide_title=true)
